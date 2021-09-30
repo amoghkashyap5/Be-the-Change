1dd7ed23cf443ccf205d656efed2ce8950e6cf46
Amogh <amoghkashyap365@gmail.com>
2021-09-30 19:54:39 -0400

    Initial Commit

---
+++ Be the Change/Be_the_ChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Be_the_ChangeApp.swift
+//  Be the Change
+//
+//  Created by Amogh Kashyap on 9/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Be_the_ChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Be the Change/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Be the Change
+//
+//  Created by Amogh Kashyap on 9/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
